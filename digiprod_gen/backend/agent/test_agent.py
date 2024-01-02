2158540a200bc97b8c52b3e59204604005e60089
fteutsch <florianteutsch@yahoo.de>
2024-01-02 18:16:41 +0100

    Small improvement for ai agent tools

--- digiprod_gen/backend/agent/test_agent.py
+++ digiprod_gen/backend/agent/test_agent.py
@@ -1,21 +1,33 @@
 import os
 import streamlit as st
-from digiprod_gen.backend.agent.tools.product import select_mba_products, print_select_mba_products
-from digiprod_gen.backend.agent.tools.crawling import get_crawling_request, crawl_mba
+from functools import partial
 from langchain.llms import OpenAI
 from langchain.agents import initialize_agent, AgentType
 from langchain.tools.render import format_tool_to_openai_function
+from langchain.tools import StructuredTool
 
+from digiprod_gen.backend.agent.tools.product import select_mba_products, print_select_mba_products
+from digiprod_gen.backend.agent.tools.crawling import crawl_mba
+from digiprod_gen.backend.agent.models.api import CrawlingMBARequest
+from digiprod_gen.backend.api.common import CONFIG
+from digiprod_gen.frontend.backend_caller import BackendCaller
 
 def init_environment():
     os.environ["OPENAI_API_KEY"] = st.secrets["api_token"]["open_ai"]
     os.environ["REPLICATE_API_TOKEN"] = st.secrets["api_token"]["replicate"]
 
+
+#backend_caller = BackendCaller(CONFIG.backend)
+#crawl_mba_tool = StructuredTool.from_function(partial(crawl_mba, backend_caller=backend_caller))
+# crawl_mba_tool = StructuredTool.from_function(func=lambda kwargs: crawl_mba(**kwargs, backend_caller=backend_caller),
+#                                               name="crawlMBATool",
+#                                               description="use to crawl amazon mba and receive mba products",
+#                                               args_schema=CrawlingMBARequest)
+
 tools = [
-    get_crawling_request,
-    crawl_mba,
     select_mba_products,
-    print_select_mba_products
+    print_select_mba_products,
+    crawl_mba
 ]
 
 if __name__ == "__main__":
@@ -43,7 +55,7 @@ if __name__ == "__main__":
     #     verbose=True
     # )
 
-    agent_executor.invoke({"input": "Create a mba request with search term 'Unicorn' and crawl some mba products. Select a subsample by using one of your tools. Print selected subsample at the end."})
+    agent_executor.invoke({"input": "Create a mba request with search term 'Unicorn' and crawl some mba products with the response you receive from the function named 'getMbaRequestTool'. Select a subsample by using one of your tools. Print selected subsample at the end."})
 
     #print(agent.agent.llm_chain.prompt.template)
     #print(agent_executor.run("Crawl some mba products and select 4 which you find suitable."))
